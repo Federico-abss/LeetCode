b64dfbe0bcd3b1cb6cb8cca0fcc8a3748653fa00
Federico Mannucci <federicomannucci@yahoo.it>
2021-01-01 11:36:48 +0100

    Time: 36 ms (91.45%), Space: 14.4 MB (14.54%) - LeetHub

--- check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
+++ check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
@@ -1,3 +1,4 @@
+# single node of a linked list
 class ListNode:
     def __init__(self, val = 0, next = None):
         
@@ -8,36 +9,35 @@ class ListNode:
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
         
-        tot = 0
         dummy = ListNode()
         cur = dummy
+        hmap = {}
+        # construct a linked list out of arr
+        # and store the nodes in hashmap 
         for num in arr:
             
-            cur.next = ListNode(num)
-            cur = cur.next
-            
-        hmap = {}
-        
-        cur = dummy.next
-        while cur:
-            
-            hmap[cur.val] = cur
-            cur = cur.next
+            cur.next = ListNode(num)             
+            cur = cur.next 
+            hmap[num] = cur             
         
         for piece in pieces:
             
             cur = None
             tail = False
             for num in piece:
+                # if we have a node we follow the list and check if 
+                # each node value matches with the current piece
+                # otherwise search the hmap to find the node
                 if ((not cur and num in hmap) or (cur and num == cur.val)) and not tail:
                     cur = hmap[num]
-                    tot += 1
                     cur = cur.next
                     if not cur:
                         tail = True
-                        
+                
+                # if the hmap does not contain the num or the
+                # next values in the piece do not correspond to
+                # the continuation of the list we return false
                 else:
                     return False
             
-        
         return True
