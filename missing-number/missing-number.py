317419bb068647a11492520618d5d834909f1269
Federico Mannucci <federicomannucci@yahoo.it>
2021-03-03 09:28:55 +0100

    Time: 136 ms (54.89%), Space: 15.4 MB (54.29%) - LeetHub

---
+++ missing-number/missing-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def missingNumber(self, nums):
+        nums.sort()
+
+        # Ensure that n is at the last index
+        if nums[-1] != len(nums):
+            return len(nums)
+        # Ensure that 0 is at the first index
+        elif nums[0] != 0:
+            return 0
+
+        # If we get here, then the missing number is on the range (0, n)
+        for i in range(1, len(nums)):
+            expected_num = nums[i-1] + 1
+            if nums[i] != expected_num:
+                return expected_num
