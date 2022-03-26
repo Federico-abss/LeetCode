215f418707ea7b4d102a9fa9b4de73f73a77dced
Federico Mannucci <federicomannucci@yahoo.it>
2022-03-26 15:34:09 +0100

    Time: 3 ms (72.48%), Space: 47.9 MB (47.76%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,33 +10,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        ListNode head = l1;
-        ListNode prev = head;
-        int remainder = 0;
-        while (l1 != null && l2 != null) {
+        return recursiveHelper(l1, l2, 0);
+    }
+    
+    private ListNode recursiveHelper(ListNode l1, ListNode l2, int remainder) {
+        if (l1 != null && l2 != null) {
             remainder += l1.val + l2.val;
             l1.val = remainder % 10;
-            remainder /= 10;
-            prev = l1;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        if (l2 != null) {
-            prev.next = l2;
-            l1 = prev.next;
-        }
-        while (remainder != 0) {
-            if (l1 == null) {
-                prev.next = new ListNode(remainder);
-                break;
+            l1.next = recursiveHelper(l1.next, l2.next, remainder/10);
+            return l1;
+        } else if (l2 != null) {
+            return recursiveHelper(l2, l1, remainder);
+        } else if (l1 != null) {
+            if (remainder != 0) {
+                remainder += l1.val;
+                l1.val = remainder % 10;
+                l1.next = recursiveHelper(l1.next, l2, remainder/10);
             }
-            remainder += l1.val;
-            l1.val = remainder % 10;
-            remainder /= 10;
-            prev = l1;
-            l1 = l1.next;
+            
+            return l1;
+        } else if (remainder == 0) {
+            return null;
         }
-        return head;
+        return new ListNode(remainder);
     }
 }
