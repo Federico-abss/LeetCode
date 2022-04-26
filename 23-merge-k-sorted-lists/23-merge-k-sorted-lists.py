5c2beb088495f69042ea7085305db97aa4d3c262
Federico Mannucci <federicomannucci@yahoo.it>
2022-04-26 17:21:23 +0200

    Time: 226 ms (21.12%), Space: 17.6 MB (94.83%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,26 +4,55 @@
 #         self.val = val
 #         self.next = next
 
+setattr(ListNode, "__lt__", lambda self, other: self.val <= other.val)
+
+def mergeTwoLists(l1: ListNode, l2: ListNode):
+    
+    if not l1: 
+        return l2
+    if not l2:
+        return l1
+    
+    head = ListNode()
+    cur = head
+    
+    while l1 and l2:
+        if l1 < l2:
+            cur.next = l1
+            cur = cur.next
+            l1 = l1.next
+        else:
+            cur.next = l2
+            cur = cur.next
+            l2 = l2.next
+            
+    if l1:
+        cur.next = l1
+    else:
+        cur.next = l2 
+            
+    return head.next
+    
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        q = []
-        setattr(ListNode, "__lt__", lambda self, other: self.val <= other.val)
+        lists2 = []
         
-        for list in lists:
-            if list:
-                heappush(q, list)
-            
-        head = ListNode()
-        cur = head
+        if not lists:
+            return
         
-        while q:
-            node = heappop(q)
-            cur.next = node
-            if node.next:
-                heappush(q, node.next)
-            cur = cur.next
-            
-            
-        return head.next
+        while len(lists2) > 1 or len(lists) > 1:
+            while len(lists) > 1:
+                lists2.append(mergeTwoLists(lists.pop(), lists.pop()))
+                
+            while len(lists2) > 1:
+                lists.append(mergeTwoLists(lists2.pop(), lists2.pop()))
+                
+        
+        return mergeTwoLists(lists[0] if lists else None, lists2[0] if lists2 else None)
+        
+        
+    
+    
+    
