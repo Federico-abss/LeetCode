91cbf737140157e2baa2b062ffed4ac3dfb3ce11
Federico Mannucci <federicomannucci@yahoo.it>
2022-04-26 16:24:02 +0200

    Time: 370 ms (35.11%), Space: 15.1 MB (70.32%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
@@ -1,54 +1,12 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
+
+        nums.sort()
         
-        maxNums = []
-        minNums = []
-        closestNegToZero = []
-        
-        for num in nums:
-            if num < 0:
-                if len(minNums) < 2:
-                    minNums.append(num)
-                    minNums.sort()
-                else:
-                    if num < minNums[-1]:
-                        minNums.pop()
-                        minNums.append(num)
-                        minNums.sort()
-                        
-                if len(closestNegToZero) < 3:
-                    closestNegToZero.append(num)
-                    closestNegToZero.sort(reverse = True)
-                else:
-                    if num > minNums[-1]:
-                        closestNegToZero.pop()
-                        closestNegToZero.append(num)
-                        closestNegToZero.sort(reverse = True)
-                            
-            if num >= 0:
-                if len(maxNums) < 3:
-                    maxNums.append(num)
-                    maxNums.sort(reverse = True)
-                else:
-                    if num > maxNums[-1]:
-                        maxNums.pop()
-                        maxNums.append(num)
-                        maxNums.sort(reverse = True)
-                        
-        
-        if not maxNums:
-            print(closestNegToZero)
-            return prod(closestNegToZero)
-        
-        if len(nums) == 3:
-            return prod(nums)
-        
-        if minNums and (prod(minNums) > prod(maxNums[1:])):
-            return prod(minNums) * maxNums[0]
+        if nums[-1] >= 0 and prod(nums[0:2]) > prod(nums[-3:-1]):
+            return prod(nums[0:2]) * nums[-1]
         else:
-            return prod(maxNums)
-        
-        return 0
-        
+            return prod(nums[-3:])
+
                     
                     
