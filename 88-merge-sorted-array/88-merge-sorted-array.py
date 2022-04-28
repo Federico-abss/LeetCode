1049ffe29bdddf8156369a18f809aa06ef91666b
Federico Mannucci <federicomannucci@yahoo.it>
2022-04-28 19:06:44 +0200

    Time: 45 ms (64.36%), Space: 13.9 MB (87.08%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,10 +4,19 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        idx2 = 0
-        for idx in range(m, len(nums1)):
-            nums1[idx] = nums2[idx2]
-            idx2 += 1
+        idx2 = cur = 0
+        temp = []
+        
+        for num in nums1[:m]:
+            while idx2 < n and nums2[idx2] <= num:
+                temp.append(nums2[idx2])
+                idx2 += 1
                 
-        nums1.sort()
+            temp.append(num)
+            
+        temp += nums2[idx2:]
+        for idx, num in enumerate(temp):
+            nums1[idx] = num
+                
+        
                 
