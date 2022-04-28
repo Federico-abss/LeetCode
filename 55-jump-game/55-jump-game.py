b1080a1cc9b65566feac9582e041c6d8d403347d
Federico Mannucci <federicomannucci@yahoo.it>
2022-04-28 17:40:25 +0200

    Time: 9155 ms (5.00%), Space: 45.1 MB (5.00%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        if len(nums) == 1:
+            return True
+        
+        @lru_cache(maxsize=None)
+        def jumps(idx: int, jump: int) -> bool:
+            if not jump:
+                return False
+
+            if idx + jump >= len(nums) - 1:
+                return True
+
+            for num in range(jump, 0, -1):
+                if jumps(idx + num, nums[idx+num]):
+                    return True
+
+            return False
+        
+        return jumps(0, nums[0])
+        
+
+        
