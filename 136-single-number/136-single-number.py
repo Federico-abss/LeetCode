6ebb20b4d6f2f75e50416feb94c5c45c94b8e9bf
Federico Mannucci <federicomannucci@yahoo.it>
2022-04-29 11:23:04 +0200

    Time: 181 ms (53.22%), Space: 16.7 MB (85.25%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        acc = 0
-        
-        for num in nums:
-            acc = acc ^ num
-            
-        return acc
+        return reduce(operator.xor, nums)
         
