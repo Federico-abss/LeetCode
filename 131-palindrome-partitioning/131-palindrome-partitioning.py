d3bd64554d5d679f53da4f85174a0ca9d42cf5b6
Federico Mannucci <federicomannucci@yahoo.it>
2022-05-02 11:55:13 +0200

    Time: 1052 ms (20.73%), Space: 32.3 MB (15.70%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,21 @@
+def isPalindrome(s: str) -> bool:
+    return s == s[::-1]
+    
+        
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        solutions = defaultdict(list)
+        solutions[0] = [[]]
+        for idx in range(len(s)):
+            offset = idx+1
+            while offset <= len(s):
+                if isPalindrome(s[idx:offset]):
+                    solutions[offset].extend([solution + [s[idx:offset]] for solution in solutions[idx]])
+
+                offset += 1
+        
+        return solutions[len(s)]
+        
+        
+            
