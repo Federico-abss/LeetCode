55b6887cc2babe2d25db484d533372a79a38cf1e
Federico Mannucci <federicomannucci@yahoo.it>
2022-05-10 11:19:56 +0200

    Time: 884 ms (9.45%), Space: 13.9 MB (41.74%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        
+        def isPalindrome(subs: str) -> bool:
+            return subs == subs[::-1]
+                    
+        for idx in range(len(s)):
+            for jdx in range(idx, len(s)):
+                count += isPalindrome(s[idx:jdx+1])
+                    
+        return count
+                
+        
