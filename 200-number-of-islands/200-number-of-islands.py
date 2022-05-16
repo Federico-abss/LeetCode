39885ce60969fb24b24cc3a2a4c29518cec4d1f0
Federico Mannucci <federicomannucci@yahoo.it>
2022-05-16 21:05:06 +0200

    Time: 386 ms (59.36%), Space: 17.7 MB (34.14%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,28 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        islands = 0
+        
+        if len(grid) == 0: return 0
+        
+        row = len(grid) 
+        col = len(grid[0])
+        
+        self.count = sum(grid[i][j]=='1' for i in range(row) for j in range(col))
+        parent = [-1 for _ in range(row*col)]
+        
+        def find(x):
+            if parent[x]!= -1:
+                return find(parent[x])
+            return x
+        
+        def union(x,y):
+            xroot, yroot = find(x),find(y)
+            if xroot == yroot: return 
+            parent[yroot] = xroot
+            self.count -= 1
         
         
-        def explore(x: int, y: int):
-            if x >= n or y >= m or x < 0 or y < 0:
-                return
-            
-            if grid[y][x] == "1":
-                grid[y][x] = "0"
-                explore(x, y+1)
-                explore(x, y-1)
-                explore(x+1, y)
-                explore(x-1, y)
-                
         
-        for y, row in enumerate(grid):
-            for x, cell in enumerate(row):
-                if cell == "1":
-                    islands += 1
-                    explore(x, y)
-                
-        return islands
-        
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '0':
+                    continue
+                index = i*col + j
+                if j < col-1 and grid[i][j+1] == '1':
+                    union(index, index+1)
+                if i < row-1 and grid[i+1][j] == '1':
+                    union(index, index+col)
+        return self.count
                 
