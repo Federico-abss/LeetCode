c8cc2d409a9fa19cae78c3dd9314ddc4afb1b34f
Federico Mannucci <federicomannucci@yahoo.it>
2022-05-17 12:29:41 +0200

    Time: 34 ms (91.53%), Space: 13.9 MB (29.88%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        for y, row in enumerate(matrix):
+            if y >= n // 2:
+                break
+            for x, num in enumerate(row[y:-y-1], y):
+                matrix[y][x] = matrix[n-1-x][y]
+                matrix[n-1-x][y] = matrix[n-y-1][m-x-1] 
+                matrix[n-y-1][m-x-1] = matrix[x][m-y-1]
+                matrix[x][m-y-1] = num
+               
+            
+                
