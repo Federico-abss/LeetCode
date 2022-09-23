fc3d23e99c080677c386967cececa301303726d1
Federico Mannucci <federicomannucci@yahoo.it>
2022-09-23 10:31:26 +0200

    Time: 3815 ms (5.09%), Space: 15.4 MB (89.91%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         @lru_cache
-        def dfs(x, y):
+        def minSearch(x, y):
             if x == len(grid[0]) - 1 and y == len(grid) - 1:
                 return grid[y][x]
             
             elif x >= len(grid[0]) or y >= len(grid):
                 return float("inf")
             
-            return grid[y][x] + min(dfs(x+1, y), dfs(x, y+1))
+            return grid[y][x] + min(minSearch(x+1, y), minSearch(x, y+1))
                 
-        return dfs(0,0)
+        return minSearch(0,0)
