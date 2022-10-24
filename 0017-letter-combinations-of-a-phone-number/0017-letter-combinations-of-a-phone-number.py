c7617a86b8fb613d483dcf444adbddd47cc7d538
Federico Mannucci <federicomannucci@yahoo.it>
2022-10-24 11:28:19 +0200

    Time: 33 ms (92.42%), Space: 13.9 MB (79.62%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        if not digits:
+            return 
+        
+        result = []
+        mapping = {"2": ["a","b","c"], "3": ["d","e","f"], "4": ["g","h","i"], 
+                   "5": ["j","k","l"], "6": ["m","n","o"], "7": ["p","q","r","s"], 
+                   "8": ["t","u","v"], "9": ["w","x","y","z"]}
+        
+        def helper(digits, cur):
+            if not digits:
+                result.append(cur)
+                return
+            
+            for number in mapping[digits[0]]:
+                helper(digits[1:], cur + number)
+        
+        helper(digits, "")
+        return result
